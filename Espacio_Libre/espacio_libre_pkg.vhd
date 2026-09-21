be7a2190d3b3e4eeee7a8741e33eb06f0d6dc4d1
Pre-ING MiguelP. <miguelangel2610o@gmail.com>
2026-09-21 08:00:00 -0500

    Creacion de pkg
    Estoy en duda si dejar el divisor_frecuencia ahi.
    Codigo creado por mi

---
+++ Espacio_Libre/espacio_libre_pkg.vhd
@@ -0,0 +1,51 @@
+LIBRARY IEEE;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.STD_LOGIC_1164.ALL;
+
+package espacio_libre_pkg is
+	
+	COMPONENT caso_1_alerta is
+	
+		PORT(
+				clk              : in  STD_LOGIC;
+				reset            : in  STD_LOGIC;
+				pulso_1s         : in  STD_LOGIC; -- El "tic" que viene de nuestro divisor
+				sensor_presencia : in  STD_LOGIC; -- '1' = Ocupado, '0' = Libre
+        
+				led_alarma       : out STD_LOGIC;
+				conteo_base      : out integer range 0 to 35;  -- Muestra los primeros 35s
+				conteo_extra     : out integer range 0 to 999  -- Muestra el tiempo extra a facturar
+				);  
+				
+	END COMPONENT;
+	
+	COMPONENT caso_felicitacion is 
+		
+			PORT(
+					clk              : in  STD_LOGIC;
+					reset            : in  STD_LOGIC;
+					pulso_1s         : in  STD_LOGIC; -- El "tic" que viene de nuestro divisor
+					sensor_presencia : in  STD_LOGIC; -- '1' = Ocupado, '0' = Libre
+					
+					led_felicitacion : out STD_LOGIC; -- Por medio de un LED se avisara de la felicitacion
+					conteo_base      : out integer range 0 to 35 -- Muestra los primeros 35s
+					); 
+					
+	END COMPONENT;
+	
+	COMPONENT divisor_frecuecia is
+			
+			GENERIC(
+				 -- Esto hace que el código sea reutilizable. 
+				 -- Si tu FPGA es de 50MHz, cuenta hasta 50,000,000 para lograr 1 segundo.
+				 F_RELOJ : integer := 50000000 
+				 );
+			
+			PORT(
+					clk      : in  STD_LOGIC;
+					reset    : in  STD_LOGIC;
+					pulso_1s : out STD_LOGIC -- Representa los flancos de subida del clk 
+					);
+			 
+	END COMPONENT;
