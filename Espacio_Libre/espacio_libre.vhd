3fc0b3f353a8209d10038ea3a44c49ca40603951
Pre-ING MiguelP. <miguelangel2610o@gmail.com>
2026-09-22 07:27:43 -0500

    Cambios en espacio_libre
    Se agregaron entradas en la entidad.
    Se asignaros las señales a utilizar en la arquitectura.
    Algunos cambios de sintaxis
    Codigo hecho por mi con ayuda de la IA para verificar que lo que estoy haciendo esta bien

--- Espacio_Libre/espacio_libre.vhd
+++ Espacio_Libre/espacio_libre.vhd
@@ -1,7 +1,36 @@
 LIBRARY IEEE;
 
-use IEEE.STD_LOGIC_ARITH.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
-use IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.STD_LOGIC_ARITH.ALL;
+USE IEEE.STD_LOGIC_UNSIGNED.ALL;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE work.espacio_libre_pkg.ALL;
 
-entity 
+ENTITY espacio_libre is
+	port(
+	
+			clk_fpga 		  : IN STD_LOGIC;
+			btn_reset 		  : IN STD_LOGIC;
+			sensor 			  : IN STD_LOGIC;
+			
+			led_alarma 		  : OUT STD_LOGIC;
+			led_felicitacion : OUT STD_LOGIC;
+			disp_unidades    : OUT STD_LOGIC_VECTOR (6 downto 0);
+			disp_decenas 	  : OUT STD_LOGIC_VECTOR (6 downto 0);
+			disp_centenas 	  : OUT STD_LOGIC_VECTOR (6 downto 0);
+			disp_miles 		  : OUT STD_LOGIC_VECTOR (6 downto 0)
+			);
+			
+END ENTITY;
+
+architecture procedimiento of espacio_libre is
+
+			SIGNAL clk_1HZ		  : STD_LOGIC := '0';
+			SIGNAL s_unidades   : INTEGER range 0 to 9;
+			SIGNAL s_decenas    : INTEGER range 0 to 9;
+			SIGNAL s_centenas   : INTEGER range 0 to 9;
+			SIGNAL s_miles 	  : INTEGER range 0 to 9;
+			SIGNAL dec_unidades : STD_LOGIC_VECTOR (3 downto 0);
+			SIGNAL dec_decenas  : STD_LOGIC_VECTOR (3 downto 0);
+			SIGNAL dec_centenas : STD_LOGIC_VECTOR (3 downto 0);
+			SIGNAL dec_miles 	  : STD_LOGIC_VECTOR (3 downto 0);
+			
