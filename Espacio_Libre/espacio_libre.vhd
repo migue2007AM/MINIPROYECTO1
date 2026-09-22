a45d621001f5f62a9fb7037067d8bb44e2b530ee
Pre-ING MiguelP. <miguelangel2610o@gmail.com>
2026-09-22 08:14:42 -0500

    Cambios en espacio_libre
    He estado viendo que SIGNAL necesito en la arquitectura 'procedimiento' pero antes de seguir con el mapeo considere mejor unir primero los dos casos para que el armado en espacio_libre al ser top-level quede más limpio

--- Espacio_Libre/espacio_libre.vhd
+++ Espacio_Libre/espacio_libre.vhd
@@ -6,7 +6,11 @@ USE IEEE.STD_LOGIC_1164.ALL;
 USE work.espacio_libre_pkg.ALL;
 
 ENTITY espacio_libre is
-	port(
+	GENERIC(
+				freq_fpga : integer := 50000000
+				);
+	
+	PORT(
 	
 			clk_fpga 		  : IN STD_LOGIC;
 			btn_reset 		  : IN STD_LOGIC;
@@ -24,13 +28,48 @@ END ENTITY;
 
 architecture procedimiento of espacio_libre is
 
-			SIGNAL clk_1HZ		  : STD_LOGIC := '0';
-			SIGNAL s_unidades   : INTEGER range 0 to 9;
-			SIGNAL s_decenas    : INTEGER range 0 to 9;
-			SIGNAL s_centenas   : INTEGER range 0 to 9;
-			SIGNAL s_miles 	  : INTEGER range 0 to 9;
-			SIGNAL dec_unidades : STD_LOGIC_VECTOR (3 downto 0);
-			SIGNAL dec_decenas  : STD_LOGIC_VECTOR (3 downto 0);
-			SIGNAL dec_centenas : STD_LOGIC_VECTOR (3 downto 0);
-			SIGNAL dec_miles 	  : STD_LOGIC_VECTOR (3 downto 0);
+			SIGNAL clk_1HZ		   : STD_LOGIC := '0';
+			SIGNAL s_unidades    : INTEGER range 0 to 9;
+			SIGNAL s_decenas     : INTEGER range 0 to 9;
+			SIGNAL s_centenas    : INTEGER range 0 to 9;
+			SIGNAL s_miles 	   : INTEGER range 0 to 9;
+			SIGNAL dec_unidades  : STD_LOGIC_VECTOR (3 downto 0);
+			SIGNAL dec_decenas   : STD_LOGIC_VECTOR (3 downto 0);
+			SIGNAL dec_centenas  : STD_LOGIC_VECTOR (3 downto 0);
+			SIGNAL dec_miles 	   : STD_LOGIC_VECTOR (3 downto 0);
+			SIGNAL s_conteobase  : INTEGER range 0 to 35;
+			SIGNAL s_conteoextra : INTEGER range 0 to 999;
+			
+			U_divisor_frecuencia : divisor_frecuencia
+					GENERIC MAP(
+									F_RELOJ => freq_fpga
+									);
+					
+					PORT MAP(
+								clk => clk_fpga,
+								reset => btn_reset,
+								pulso_1s => clk_1HZ
+								);
+								
+			U_caso1 : caso_1_alerta
+					PORT MAP(
+								clk => clk_fpga,
+								reset => btn_reset,
+								sensor_presencia => sensor,
+								
+								led_alarma => led_alarma,
+								conteo_base => s_conteobase,
+								conteo_extra => s_conteoextra
+								);
+								
+			U_caso2 : caso2_felicitacion
+					PORT MAP(
+								clk => clk_fpga,
+								reset => btn_reset,
+								sensor_presencia => sensor,
+								
+								led_felicitacino => led_felicitacion,
+								conteo_base => s_conteobase
+								);
+								
 			
