3a3798df1cc6499486f43598e7d67f8dc81f2cb9
Pre-ING MiguelP. <miguelangel2610o@gmail.com>
2026-09-22 23:23:27 -0500

    Cambios previos del top-level
    Quite lo de centenas y miles ya que no se va a usar por lo de que solo seran unidades y decenas.
    Tambien quite los mapeos de caso 1 y caso 2 porque ya los unifique en un solo .vhd

--- Espacio_Libre/espacio_libre.vhd
+++ Espacio_Libre/espacio_libre.vhd
@@ -18,9 +18,7 @@ ENTITY espacio_libre is
 			led_alarma 		  : OUT STD_LOGIC;
 			led_felicitacion : OUT STD_LOGIC;
 			disp_unidades    : OUT STD_LOGIC_VECTOR (6 downto 0);
-			disp_decenas 	  : OUT STD_LOGIC_VECTOR (6 downto 0);
-			disp_centenas 	  : OUT STD_LOGIC_VECTOR (6 downto 0);
-			disp_miles 		  : OUT STD_LOGIC_VECTOR (6 downto 0)
+			disp_decenas 	  : OUT STD_LOGIC_VECTOR (6 downto 0)
 			);
 			
 END ENTITY;
@@ -34,8 +32,6 @@ architecture procedimiento of espacio_libre is
 			SIGNAL s_miles 	   : INTEGER range 0 to 9;
 			SIGNAL dec_unidades  : STD_LOGIC_VECTOR (3 downto 0);
 			SIGNAL dec_decenas   : STD_LOGIC_VECTOR (3 downto 0);
-			SIGNAL dec_centenas  : STD_LOGIC_VECTOR (3 downto 0);
-			SIGNAL dec_miles 	   : STD_LOGIC_VECTOR (3 downto 0);
 			SIGNAL s_conteobase  : INTEGER range 0 to 35;
 			SIGNAL s_conteoextra : INTEGER range 0 to 999;
 			
@@ -48,27 +44,5 @@ architecture procedimiento of espacio_libre is
 								clk => clk_fpga,
 								reset => btn_reset,
 								pulso_1s => clk_1HZ
-								);
-								
-			U_caso1 : caso_1_alerta
-					PORT MAP(
-								clk => clk_fpga,
-								reset => btn_reset,
-								sensor_presencia => sensor,
-								
-								led_alarma => led_alarma,
-								conteo_base => s_conteobase,
-								conteo_extra => s_conteoextra
-								);
-								
-			U_caso2 : caso2_felicitacion
-					PORT MAP(
-								clk => clk_fpga,
-								reset => btn_reset,
-								sensor_presencia => sensor,
-								
-								led_felicitacino => led_felicitacion,
-								conteo_base => s_conteobase
-								);
 								
 			
