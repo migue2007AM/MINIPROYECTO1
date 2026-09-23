966c1ae478668d001638a129d2ee573a98c35fc4
Pre-ING MiguelP. <miguelangel2610o@gmail.com>
2026-09-23 00:12:41 -0500

    Cambio en union_contadores
    Quite todo lo que tenia que ver con el componente contador_individual porque ya no lo estaba usando, además de por si cambie su logica, ahora simplemente recibe un numero (que en este caso es la salida de conteo de dos_casos) y lo separa en unidades y en decenas

--- Espacio_Libre/union_contadores.vhd
+++ Espacio_Libre/union_contadores.vhd
@@ -7,55 +7,20 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity union_contadores is
 
 		port(
-				clk      : in STD_LOGIC;
-				reset    : in STD_LOGIC;
-				enable   : in STD_LOGIC;
+				numero_entrada : IN integer range 0 to 99;
 				
-				unidades : out integer range 0 to 9;
-				decenas  : out integer range 0 to 9
+				unidades 		: out integer range 0 to 9;
+				decenas  		: out integer range 0 to 9
 				);
 			
 end entity;
 
 architecture conteo of union_contadores is
 
-		signal carry_uni : STD_LOGIC;
-		signal carry_dec : STD_LOGIC;
+begin
+		decenas <= numero_entrada / 10;
 		
-		COMPONENT contador_individual is
-		
-			port(
-					clk       : in STD_LOGIC;
-					reset     : in STD_LOGIC;	-- '1' para activo, '0' para desactivado
-					enable 	 : in STD_LOGIC;
-			
-					carry_out : out STD_LOGIC;	
-					cuenta 	 : out integer range 0 to 9
-					);
-						
-		end COMPONENT;
-		
-		begin
-				U_UNIDADES : contador_individual 
-					port map(
-								clk 		 => clk,
-								reset 	 => reset,
-								enable 	 => enable,
-								
-								carry_out => carry_uni,
-								cuenta    => unidades
-								);
-				
-				U_DECIMAS : contador_individual
-					port map(
-								clk       => clk,
-								reset 	 => reset,
-								enable 	 => carry_uni,
-								
-								carry_out => open,
-								cuenta 	 => decimas
-								);
-								
+		unidades <= numero_entrada mod 10;							
 					
 end conteo;
 
