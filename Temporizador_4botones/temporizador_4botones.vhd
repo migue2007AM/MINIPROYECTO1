bd87360b6c185fd1a96ceabe703d53d25d166789
Pre-ING MiguelP. <miguelangel2610o@gmail.com>
2026-09-25 22:33:33 -0500

    Creacion de temporizador_4botones (top-level)
    Primera version con apoyo de Gemini 3.1 Pro

---
+++ Temporizador_4botones/temporizador_4botones.vhd
@@ -0,0 +1,78 @@
+LIBRARY IEEE;
+LIBRARY WORK;
+
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.STD_LOGIC_UNSIGNED.ALL;
+USE IEEE.STD_LOGIC_ARITH.ALL;
+USE WORK.temporizador_4botones_pkg.ALL;
+
+ENTITY temporizador_4botones is
+	PORT(	
+			clk : IN STD_LOGIC;
+			start : IN STD_LOGIC;
+			stop : IN STD_LOGIC;
+			reset : IN STD_LOGIC;
+			
+			disp_sec1 : OUT STD_LOGIC_VECTOR(6 downto 0);
+			disp_sec2 : OUT STD_LOGIC_VECTOR(6 downto 0);
+			disp_min : OUT STD_LOGIC_VECTOR(6 downto 0)
+			
+		);
+		
+END ENTITY;
+
+ARCHITECTURE conteo OF temporizador_4botones IS
+
+	SIGNAL running : STD_LOGIC;
+	SIGNAL clk_1HZ : STD_LOGIC;
+	SIGNAL carry1  : STD_LOGIC;
+	SIGNAL carry2  : STD_LOGIC;
+	
+	U_divisor_frecuencia : divisor_frecuencia
+		GENERIC MAP(
+						FININ => 49999999,
+						FOUT => 1
+						)
+						
+		PORT MAP(
+					clk_in => clk,
+					reset => reset,
+					clk_out => clk_1HZ
+				);
+				
+	U_UNIDADES_SEC : MOD_N
+		PORT MAP(
+					clk => clk_1HZ,
+					reset => reset,
+					enable => start,
+					
+					carry => carry1,
+					seg7_out => disp_sec1
+				);
+				
+	U_DECENAS_SEC : MOD_N
+		GENERIC MAP(
+						limit => 5
+					)
+		
+		PORT MAP(
+					clk => clk_1HZ,
+					reset => reset,
+					enable => carry1,
+					
+					carry => carry2,
+					seg7_out => disp_sec2
+				);
+				
+	U_UNIDADES_MIN : MOD_N
+		PORT MAP(
+					clk => clk_1HZ,
+					reset => reset,
+					enable => carry2,
+					
+					carry => open,
+					seg7_out => disp_min
+				);
+				
+END conteo;
+
