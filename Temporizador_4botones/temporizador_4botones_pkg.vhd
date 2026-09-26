9006d92615fe346a36bec1c005c5632a79bfd2ff
Pre-ING MiguelP. <miguelangel2610o@gmail.com>
2026-09-25 22:53:51 -0500

    Entrada no utilizada
    Se quita esta entrada ya que los MOD_N no necesitan sabe cuando se habilita el running, solo que les llegue las habilitaciones de los carry, solo el U_UNIDADES_SEC necesitaria el running en enable para saber cuando inicia el sistema

--- Temporizador_4botones/temporizador_4botones_pkg.vhd
+++ Temporizador_4botones/temporizador_4botones_pkg.vhd
@@ -15,7 +15,6 @@ PACKAGE temporizador_4botones_pkg is
 					clk      	 	 : IN STD_LOGIC; -- esta entrada recibe la señal de reloj del divisor de frecuencia
 					reset    		 : IN STD_LOGIC; -- esta entrada si es '1' el sistema se reinicia, en '0' no se reinicia
 					enable   		 : IN STD_LOGIC; -- esta entrada se encarga de dar luz verde para iniciar el conteo
-					running  		 : IN STD_LOGIC; -- esta entrada lo que hace es recordar la señal que envio el boton pulsado, sea start o stop, esto soluciona el problema de no mantener presionado el boton 
 					
 					carry    		 : OUT STD_LOGIC; -- esta salida sirve para dar luz verde pero a otro MOD_N
 					seg7_out 		 : OUT STD_LOGIC_VECTOR(6 downto 0) -- esta salida guardara la conversion directa de integer a los 7 segmentos para poder conectar directamente al display
